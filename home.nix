b65b1cdb446dcef07e4606bff823a26f96ac198f
Nathan Graule <solarliner@gmail.com>
2022-01-09 19:45:22 +0100

    feat(nvim): nerdtree config + key bindings

--- home.nix
+++ home.nix
@@ -274,6 +274,15 @@
           " Start NERDTree. If a file is specified, move the cursor to its window.
           autocmd StdinReadPre * let s:std_in=1
           autocmd VimEnter * NERDTree | if argc() > 0 || exists("s:std_in") | wincmd p | endif
+
+          " If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
+          autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
+              \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
+
+          nnoremap <leader>n :NERDTreeFocus<CR>
+          nnoremap <C-n> :NERDTree<CR>
+          nnoremap <C-t> :NERDTreeToggle<CR>
+          nnoremap <C-f> :NERDTreeFind<CR>
           '';
       }
       nerdtree-git-plugin
