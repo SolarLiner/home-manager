90cebe570801c47962660b97ed4b9e17b8fc6c52
Nathan Graule <solarliner@gmail.com>
2022-01-15 13:16:24 +0100

    feat(nvim): add clangd lsp

--- nvim/lspconfig.lua
+++ nvim/lspconfig.lua
@@ -1,6 +1,6 @@
 local nvim_lsp = require('lspconfig')
 
-local util = require "lspconfig".util
+local util = nvim_lsp.util
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -53,7 +53,7 @@ local capabilities = require('cmp_nvim_lsp')
   .update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'pyright', 'sumneko_lua' }
+local servers = { 'rnix', 'clangd', 'tsserver', 'pyright', 'sumneko_lua' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     capabilities = capabilities,
